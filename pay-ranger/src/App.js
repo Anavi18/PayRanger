e426abdc0ec958f1a7b6b1244761499c8bf58db8
Long Huynh <longbhuynh019@gmail.com>
2023-03-06 15:33:54 -0500

    login page created

--- pay-ranger/src/App.js
+++ pay-ranger/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/LogIn/LogIn.js'
 
 function App() {
   return (
     <div className="App">
      PayRanger Project
+     <Login />
     </div>
   );
 }
---
+++ pay-ranger/src/components/LogIn/LogIn.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import "./login.css";
+
+
+
+export default function LogIn({ changeRoute, loadUser }) {
+    let [username, setUsername] = useState("");
+    let [password, setPassword] = useState("");
+  
+    const onUsernameChange = (event) => {
+      setUsername(event.target.value);
+    };
+  
+    const onPasswordChange = (event) => {
+      setPassword(event.target.value);
+    };
+  
+    const handleErrrors = (res) => {
+      if (!res.ok){
+        alert("Incorrect Username/Password");
+        throw Error(res.statusText)
+      }
+      else{
+        return res.json();
+      }
+      }
+  
+    const onSubmitSignIn = () => {
+      if(username.length === 0 || password.length === 0){
+        alert("Missing field")
+      }
+      else{
+        loadUser({
+          username: username,
+          password: password
+        });
+        changeRoute(3);
+      }
+    }
+  
+    return (
+      <div className="login_wrapper">
+        <div id = "cover" className="login">
+          <h1 id="welcome_back">User Login</h1>
+          <input
+            type="text"
+            placeholder="Username"
+            onChange={onUsernameChange}
+            className="username"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={onPasswordChange}
+            className="password"
+          />
+          <div className = "row">
+          <button className="col-6 loginButton" >
+            Sign In
+          </button>
+          <button className="col-6 loginButton" >
+            
+              Register
+           
+          </button>
+  
+          </div>
+          
+          <button className="loginButton" >
+            Forgot Password
+          </button>
+        </div>
+      </div>
+    );
+  }
