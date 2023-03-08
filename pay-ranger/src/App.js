2765098bae468e81ce450724b948ddf89c91690f
Eddie <ehines@umass.edu>
2023-03-08 16:30:20 -0500

    Turned the header bar into a componant

--- pay-ranger/src/App.js
+++ pay-ranger/src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/LogIn/LogIn.js'
+import Login from './components/LogIn/LogIn.js';
+import Header from './components/Header/Header.js';
 
 function App() {
   return ( 
   <div className="App">
-     <div className="App-header">
-      Duck Creek Technologies - Employee Self Service
-    </div>
+     <Header />
      <Login />
   </div>
   );
