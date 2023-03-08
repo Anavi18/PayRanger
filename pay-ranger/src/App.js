5bbc7f3a6d6634589ef8ce5b8da9f6b1ee96c827
Eddie <ehines@umass.edu>
2023-03-08 14:05:04 -0500

    Created a header on the login page

--- pay-ranger/src/App.js
+++ pay-ranger/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Login from './components/LogIn/LogIn.js'
 
 function App() {
-  return (
-    <div className="App">
-     PayRanger Project
-     <Login />
+  return ( 
+  <div className="App">
+     <div className="App-header">
+      Duck Creek Technologies - Employee Self Service
     </div>
+     <Login />
+  </div>
   );
 }
 
