52b55063eaca93cafe2653c720ee998e4f26fdc7
Long Huynh <longbhuynh019@gmail.com>
2023-03-20 15:42:12 -0400

    updated homepage

--- pay-ranger/src/App.js
+++ pay-ranger/src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/LogIn/LogIn.js';
 import Header from './components/Header/Header.js';
+import Home from './components/HomePage/Home.js'
 
 function App() {
   return ( 
   <div className="App">
       <div className="imgShadow">
+        <Home />
         <Header />
         <Login />
+        
       </div>
   </div>
   );
