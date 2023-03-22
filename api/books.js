28561f29f2eae84e8490a47728c0f7cb759b0841
Anavi <45989213+Anavi18@users.noreply.github.com>
2023-03-22 13:24:24 -0400

    Update books.js

--- api/books.js
+++ api/books.js
@@ -1,1 +1,34 @@
+// routes/api/books.js
 
+const express = require('express');
+const router = express.Router();
+
+// Load Book model
+const Book = require('../../models/Books');
+
+// @route GET api/books/test
+// @description tests books route
+// @access Public
+router.get('/test', (req, res) => res.send('book route testing!'));
+
+// @route GET api/books/:id
+// @description Get single book by id
+// @access Public
+router.get('/:id', (req, res) => {
+  Book.findById(req.params.id)
+    .then(book => res.json(book))
+    .catch(err => res.status(404).json({ nobookfound: 'No Book found' }));
+});
+
+// @route GET api/books/:id
+// @description Update book
+// @access Public
+router.put('/:id', (req, res) => {
+  Book.findByIdAndUpdate(req.params.id, req.body)
+    .then(book => res.json({ msg: 'Updated successfully' }))
+    .catch(err =>
+      res.status(400).json({ error: 'Unable to update the Database' })
+    );
+});
+
+module.exports = router;
