9575609b5df99e89d79838bd8025ee8c5cb1728e
Long Huynh <longbhuynh019@gmail.com>
2023-03-27 15:18:54 -0400

    updated

--- pay-ranger/src/App.js
+++ pay-ranger/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Login from './components/LogIn/LogIn.js';
 import Header from './components/Header/Header.js';
 import Home from './components/HomePage/Home.js'
+import EnterTime from './components/EnterTime.js'
 
 function App() {
   return ( 
   <div className="App">
       <div className="imgShadow">
+        <EnterTime/>
         <Home />
         <Header />
         <Login />
---
+++ pay-ranger/src/components/EnterTime.js
@@ -0,0 +1,15 @@
+import React, {Component,  useState } from "react";
+import TimePicker from 'react-time-picker';
+
+class EnterTime extends Component{
+    render(){
+        return (
+            <div className="row">
+                <h2>Enter Time</h2>
+                
+            </div>
+        )
+    }
+}
+
+export default EnterTime
