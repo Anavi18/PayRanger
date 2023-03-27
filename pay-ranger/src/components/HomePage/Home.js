4bbb5de16b017c4a176a9826678d45ac3d7c3a8a
Jase <102929326+jrcanuel@users.noreply.github.com>
2023-03-27 13:43:33 -0400

    Minor changes to js to integrate css

--- pay-ranger/src/components/HomePage/Home.js
+++ pay-ranger/src/components/HomePage/Home.js
@@ -1,20 +1,23 @@
 import React, {Component} from "react";
+import "./Home.css";
 
 const EnterTime = () => {
     return (
-        <div className="card m-2" style={{width: '30em', height: '20em'}}>
+        <div className="card m-2 enterTime" style={{width: '30em', height: '20em'}}>
             <div className="card-body">
                 <h5 className="card-title">Enter Time</h5>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div className="clock"></div>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
     )
 }
-const ViewPayroll = () => {
+const ViewPayroll = () => { //not yet sure how to implement adding the images, but I've added divs to each where they should go along with the start of some css in Home.css.
     return (
-        <div className="card m-2" style={{width: '30em', height: '20em'}}>
+        <div className="card m-2 viewPayroll" style={{width: '30em', height: '20em'}}>
             <div className="card-body">
                 <h5 className="card-title">View Payroll</h5>
+                <div className="document"></div>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
@@ -23,9 +26,10 @@ const ViewPayroll = () => {
 }
 const ViewEmployee = () => {
     return (
-        <div className="card m-2" style={{width: '30em', height: '20em'}}>
+        <div className="card m-2 viewEmployee" style={{width: '30em', height: '20em'}}>
             <div className="card-body">
                 <h5 className="card-title">View Employee</h5>
+                <div className="person"></div>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
