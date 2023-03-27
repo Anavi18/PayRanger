cbf6fd439a26da662d448abd3a302a79bd1e0f17
Anavi <45989213+Anavi18@users.noreply.github.com>
2023-03-27 14:59:45 -0400

    Update books.js

--- api/books.js
+++ api/books.js
@@ -9,15 +9,15 @@ const Emp = require('../../models/Employee');
 // @route GET api/books/test
 // @description tests books route
 // @access Public
-router.get('/test', (req, res) => res.send('book route testing!'));
+router.get('/test', (req, res) => res.send('Employee record display testing!'));
 
 // @route GET api/books/:id
 // @description Get single book by id
 // @access Public
 router.get('/:employeeId', (req, res) => {
   Emp.findById(req.params.employeeId)
-    .then(book => res.json(book))
-    .catch(err => res.status(404).json({ nobookfound: 'No Book found' }));
+    .then(employees => res.json(employees))
+    .catch(err => res.status(404).json({ nobookfound: 'No Employee found' }));
 });
 
 // @route GET api/books/:id
@@ -25,7 +25,7 @@ router.get('/:employeeId', (req, res) => {
 // @access Public
 router.put('/:employeeId', (req, res) => {
   Emp.findByIdAndUpdate(req.params.employeeId, req.body)
-    .then(book => res.json({ msg: 'Updated successfully' }))
+    .then(employees => res.json({ msg: 'Updated successfully' }))
     .catch(err =>
       res.status(400).json({ error: 'Unable to update the Database' })
     );
