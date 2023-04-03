06d97fc8700a70f8e8aa75325dc06e88666322cb
Eddie <ehines@umass.edu>
2023-04-03 19:32:02 -0400

    Made dropdown appear and disappear on clicking the avatar, linked each element in the dropdown to their respective pages

--- pay-ranger/src/components/Header/Header.js
+++ pay-ranger/src/components/Header/Header.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Header.css";
 import logo from "./cropped-duck-creek-favicon-32x32.webp";
 import avatar from "./Icons/avatar.svg";
+import {Link} from "react-router-dom";
 function Header() {
+    let [dropdown, showProfileDropdown] = useState(false);
+    const toggleDropdown = (event) => {
+      showProfileDropdown(current=>!current);
+    };
     return (
       <div className="App-header">
         <div className="Logo"> 
@@ -11,21 +16,24 @@ function Header() {
         </div>
         <div className="Avatar">
           <div className="Name">Employee Name</div>
-          <div><img src={avatar} alt="Click here for employee navigation options" className="logo" /></div>
+          <div><a href="#" onClick={toggleDropdown}><img src={avatar} alt="Click here for employee navigation options" className="logo" /></a></div>
         </div>
-        <div className="profileDropdown">
-          <p>
-            Log Out
-          </p>
-          <p>
-            Enter Time
-          </p>
-          <p>
-            View Payroll
-          </p>
-          <p>
-            View Employee
-          </p>
+        <div className={dropdown ? "profileDropdown" : "profileDropdownHidden"}>
+          <div>
+            <a href="/">Log Out</a>
+          </div>
+          <div>
+            <a href="/enter-time">Enter Time</a>
+          </div>
+          <div>
+            <a href="/view-payroll">View Payroll</a>
+          </div>
+          <div>
+            <a href="/view-employee">View Employee</a>
+          </div>
+          <div>
+            <a href="/home">My Dashboard</a>
+          </div>
         </div>
       </div>
     )
