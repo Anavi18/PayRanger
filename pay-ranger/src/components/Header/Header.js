e49db8e7a9191f8b80ca14a5e0ebb290da498fc0
Eddie <ehines@umass.edu>
2023-04-03 15:49:34 -0400

    Added avatar logo to right of the header

--- pay-ranger/src/components/Header/Header.js
+++ pay-ranger/src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./Header.css";
 import logo from "./cropped-duck-creek-favicon-32x32.webp";
+import avatar from "./Icons/avatar.svg";
 function Header() {
     return (
       <div className="App-header">
-        Duck Creek Technologies 
-        <img src={logo} alt="logo" className="logo" />
+        <div className="Logo"> Duck Creek Technologies 
+         <img src={logo} alt="logo"/>
+        </div>
+        <div className="Avatar">
+          Employee Name
+          <img src={avatar} alt="Click here for employee navigation options" className="logo" />
+        </div>
       </div>
     )
 }
