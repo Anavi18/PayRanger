62092028bdf82334841f155193c4c6b25425fbe7
Eddie <ehines@umass.edu>
2023-04-10 16:22:36 -0400

    No more merge conflicts :)

--- pay-ranger/src/components/LogIn/LogIn.js
+++ pay-ranger/src/components/LogIn/LogIn.js
@@ -3,11 +3,8 @@ import "./LogIn.css";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import { useContext } from "react";
 import { LoginContext } from "../../LoginContext";
-<<<<<<< HEAD
-=======
 import Header from "../Header/Header";
 
->>>>>>> newScheme
 
 function ForgotPassword() {
   return (
@@ -19,10 +16,6 @@ export default function LogIn() {
     let [password, setPassword] = useState("");
     let [forgotPassword, showForgotPassword] = useState(false);
     let loginPair = useContext(LoginContext);
-<<<<<<< HEAD
-    console.log(loginPair);
-=======
->>>>>>> newScheme
     const onUsernameChange = (event) => {
       setUsername(event.target.value);
     };
@@ -38,39 +31,6 @@ export default function LogIn() {
     return (
       <div className="loginbg">
       <div className="login_wrapper">
-<<<<<<< HEAD
-        <div id = "cover" className="login">
-          <div className="loginFlexbox">
-            <h1 id="welcome_back">Welcome</h1>
-            <div>
-            <input
-                type="text"
-                placeholder="Username"
-                onChange={onUsernameChange}
-                className="username"
-              />
-            </div>
-            <div>
-              <input
-                type="password"
-                placeholder="Password"
-                onChange={onPasswordChange}
-                className="password"
-              />
-            </div>
-            <div className="buttonFlexbox">
-                <div><Link to="/home"><button className="loginButton" onClick={()=>{loginPair.setIsLoggedIn(current=>!current)}}>Log In</button></Link></div>
-                {/*<div><button className="regButton">Register</button></div>*/}
-            </div>
-            <div>
-               <a href="#" className="forgotPasswordLink" onClick={onForgotPasswordClick}>Forgot password?</a>
-           </div>
-           <div>
-           </div>
-          </div>  
-        </div>
-        <div className={forgotPassword ? "forgotPassword forgotPasswordExpanded" : "forgotPassword"}>{forgotPassword && <ForgotPassword/>}</div>
-=======
             <div className="loginFlexbox justify-content-center align-items-center ">
               <h1>Login</h1>
               <div className="ip">
@@ -99,7 +59,6 @@ export default function LogIn() {
           
           
         
->>>>>>> newScheme
       </div>
       </div>  
     );
